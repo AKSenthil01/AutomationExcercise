b3f017461beb94efa5126da956d153bb6a4f500b
aksenthil01 <aksenthil01@gmail.com>
2026-04-09 11:30:26 +0530

    Fix Python version for CI

--- tests/test_005_Orderconfirm.py
+++ tests/test_005_Orderconfirm.py
@@ -10,6 +10,7 @@ from pages.HomePage import HomePage
 from pages.OrderPlacedPage import OrderPlacedPage
 from pages.PaymentPage import PaymentPage
 from pages.ShopingCartPage import ShopingCartPage
+from pages.ShopingPage import ShopingPage
 from pages.SignUpPage import SignUp
 from utils.customLogger import get_custom_logger
 from core.config import get_base_url
